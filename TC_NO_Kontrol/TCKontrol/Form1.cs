3607c8e321a995608627621081e9fe2edaaa59d3
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-29 00:16:37 +0300

    Add files via upload

---
+++ TC_NO_Kontrol/TCKontrol/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sonuc = 0;
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+
+            int x = 1;
+            int y = 0;
+            int z = 0;
+            string tc = txtTC.Text;
+            tc = tc.Trim();
+            int[] sayilar = new int[11];
+            for (int i = 0; i < tc.Length; i++)
+            {
+                sayilar[i] = Int32.Parse(tc[i].ToString());
+            }
+            foreach (var item in sayilar)
+            {
+                
+                if(x < 11)
+                {
+                    sonuc = sonuc + item;
+                }
+                else
+                {
+                    z = item;
+                }
+                x++;
+            }
+            y = sonuc % 10;
+
+            if(y == z)
+            {
+                MessageBox.Show("Doğru");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış");
+            }
+            
+            
+            x = 0;
+            y = 0;
+            z = 0;
+            sonuc = 0;
+        }
+       
+       
+    }
+}
